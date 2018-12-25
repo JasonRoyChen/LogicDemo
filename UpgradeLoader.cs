b8cb4988e8c0bacf983b762c6a25377aea8a5e94
Jason Chen <suncj550@163.com>
2018-12-25 16:47:06 +0800

    Some modifications with the Upgrade logics.
    This time, all core logic to implement multi-upgrade is almost done.(Maybe some bugs still left.)

---
+++ UpgradeLoader.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using UtilityService;
+
+namespace UpgradeHost
+{
+    internal class UpgradeLoader
+    {
+        private readonly string _upgradeFolder;
+        private readonly string _usbPath;
+        private readonly string _upgradeTempFolder;
+        private readonly string _needBackup;
+        private readonly string _clientPath;
+        private readonly WizardServiceImpl _wizardService;
+        public bool IsInUpgrading { get; private set; }
+        private readonly string _title;
+        private readonly HostLoaderArgs _arguments;
+
+        private const string DoNotTrunOff = "Don't turn off the Ultrasound system, this will take a while";
+        private const string LocalUpgradePath = @"D:\\Upgrade";
+
+        public WizardServiceImpl Wizard { get { return _wizardService; } }
+
+        internal UpgradeLoader(HostLoaderArgs arguments)
+        {
+            _arguments = arguments ?? new HostLoaderArgs();
+            _upgradeFolder = _arguments.UpgradeFolder;
+            _usbPath = _arguments.Args[1];
+            _upgradeTempFolder = _arguments.Args[2];
+            _needBackup = _arguments.Args[3];
+            _clientPath = _arguments.Args[4];
+
+            _wizardService = new WizardServiceImpl(OnStartUpgrade);
+
+            _title = Translator.GetValue("Ultrasound System Update");
+        }
+        public void StartUpgrade()
+        {
+            _wizardService.Startup();
+        }
+
+        private void OnStartUpgrade()
+        {
+            IsInUpgrading = true;
+            string upgradeDescription = string.Empty;
+            string stepDes = Translator.GetValue("Working on updates\nPart 4 of 4: Upgrading to new version");
+            try
+            {
+                string destAssembly = Path.Combine(_upgradeFolder, "UpgradeHost.DLL");
+
+                if (!File.Exists(destAssembly)||!DigitalSignatureChecker.VerifySignature(destAssembly))
+                {
+                    throw new Exception();
+                }
+
+                UpgradeProxy proxy=new UpgradeProxy(destAssembly);
+                upgradeDescription = proxy.Description();
+                _wizardService.UpdateStates(_title, upgradeDescription,stepDes, DoNotTrunOff);
+                string message;
+                if (!proxy.CanExecute(out message))
+                {
+                    throw new Exception(message);
+                }
+                try
+                {
+                    Thread.Sleep(2000);
+                    proxy.Execute(_upgradeFolder,out message);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.InnerException!=null?e.InnerException.Message:e.Message);
+                }
+
+                try
+                {
+                    //delete upgrade folder
+                    //todo KnowIssue ,UpgradeHost.dll can't be delete,as it has been loaded  into AppDomain
+                    Directory.Delete(_upgradeFolder,true);
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                const string shutdownMessage = "Upgrade done successfully.\nSystem will continue automatically after";
+                string shutdownAdditionDes = string.Empty;
+                var buttons = new WizardButton[1];
+                buttons[0] = new WizardButton(WizardButtonEnum.Ok, null, "Continue");
+                int timeoutTicks = ResourceManager.GetValue("Upgrade", "TimeoutTicks", 10);
+                _wizardService.UpdateStates(_title, upgradeDescription, shutdownMessage, shutdownAdditionDes, -1, true, buttons, timeoutTicks, buttons[0]);
+
+                //Decide to start Main.exe or continue upgrade.
+                StartOrContinue();
+            }
+            catch (Exception e)
+            {
+                var buttons = new WizardButton[1];
+                buttons[0] = new WizardButton(WizardButtonEnum.Ok, null,"Shut Down");
+                _wizardService.UpdateStates(_title, upgradeDescription, Translator.GetValue("Upgrade failed.\nSystem will shut down."), e.Message, -1, true, buttons);
+                HostManager.ShutDown(RestartType.ShutdownOS);
+            }
+            finally
+            {
+                IsInUpgrading = false;
+                HostManager.ShutDown(RestartType.None);
+            }
+        }
+
+        private void StartOrContinue()
+        {
+            string upgradePackageFile = string.Empty;
+            bool isExists = false;
+
+            //Todo: 这个系统version，是否已经是最新版本？如果不是，则需要再判断时再往后跳一个等级的补丁来作为判断依据。
+            UpgradePackageService.GetCurrentVersion(out string updatedVersion, out _);
+
+            //Do new multiple upgrade.
+            if (!(string.IsNullOrEmpty(_usbPath) || _usbPath == "Null"))
+            {
+                isExists = IsUpgradeAvailable(updatedVersion, out upgradePackageFile);
+            }
+
+            if (isExists)
+            {
+                StartClient(upgradePackageFile);
+            }
+            else
+            {
+                //All upgrades are finished, start system.
+                StartSystem();
+            }
+        }
+
+        private bool IsUpgradeAvailable(string updatedVersion, out string upgradePackageFile)
+        {
+            bool result = UpgradePackageService.IsUpgradePackageExists(_usbPath, updatedVersion, out string zipFilePath);
+            if (result)
+            {
+                UpgradePackageService.ReleaseToLocal(zipFilePath);
+                //Detailed check, check the version of Resource & dat file.
+                if (UpgradePackageService.CheckUpgradeDetail(LocalUpgradePath))
+                {
+                    //Upgrade package found.
+                    upgradePackageFile = Path.Combine(LocalUpgradePath, UpgradePackageService.GetVersion(zipFilePath) + ".dat");
+                    return true;
+                }
+
+                //Try to find a higher version upgrade package.
+                updatedVersion = UpgradePackageService.GetVersion(zipFilePath);
+                if (IsUpgradeAvailable(updatedVersion, out upgradePackageFile))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                upgradePackageFile = string.Empty;
+                return false;
+            }
+
+            return false;
+        }
+
+        private void StartClient(string upgradePackageFile)
+        {
+            string arguments = string.Format("\"{0}\"\"{1}\"\"{2}\"\"{3}\"", upgradePackageFile,
+                _upgradeTempFolder, _needBackup, _usbPath);
+            string workingDirectory = Path.GetDirectoryName(_clientPath);
+
+            //Start new client.exe.
+            Process p = new Process();
+            p.StartInfo.FileName = _clientPath;
+            p.StartInfo.CreateNoWindow = false;
+            p.StartInfo.WorkingDirectory = workingDirectory;
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.Arguments = arguments;
+
+            p.Start();
+        }
+
+        private static void StartSystem()
+        {
+            const string workingDirectory = @"C:\App\Shipped\bin\";
+            const string fileName = workingDirectory + ".MainMonitor.exe";
+            if (!File.Exists(fileName))
+            {
+                throw new Exception(string.Format(Translator.GetValue("File not Exists: '{0}'."), fileName));
+            }
+
+            if (!DigitalSignatureChecker.VerifySignature(fileName))
+            {
+                throw new Exception(string.Format(Translator.GetValue("Failed to verify digital signature of the module '{0}'."), fileName));
+            }
+
+            new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName,
+                    CreateNoWindow = true,
+                    WorkingDirectory = workingDirectory,
+                    UseShellExecute = false
+                }
+            }.Start();
+        }
+    }
+}
---
+++ UpgradePackageService.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml;
+
+namespace .Services.SystemServices.Upgrade
+{
+    public static class UpgradePackageService
+    {
+        /// <summary>
+        ///     Get current software version which runs on local machine.
+        /// </summary>
+        /// <param name="revision">Send out the current version.</param>
+        /// <param name="build">Send out the build number.</param>
+        /// <returns></returns>
+        internal static void GetCurrentVersion(out string revision, out int build)
+        {
+            revision = string.Empty;
+            build = -1;
+
+            var appConfigFile = @"C:\App\Shipped\bin\.MainMonitor.exe.config";
+
+            try
+            {
+                var document = new XmlDocument();
+                document.Load(appConfigFile);
+                var node = document.SelectSingleNode("./configuration/appSettings");
+                if (node != null)
+                {
+                    var buildNumberAttribute =
+                        node.SelectSingleNode(string.Format("add[@key='{0}']/@value", "BuildNumber")) as XmlAttribute;
+
+                    var revisionAttribute =
+                        node.SelectSingleNode(string.Format("add[@key='{0}']/@value", "Revision")) as XmlAttribute;
+
+                    if (buildNumberAttribute != null && !string.IsNullOrEmpty(buildNumberAttribute.Value))
+                        int.TryParse(buildNumberAttribute.Value, out build);
+
+                    if (revisionAttribute != null) revision = revisionAttribute.Value;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(string.Format("Failed to get software revision.ex:{0}", ex));
+            }
+        }
+
+        //Validate if upgrade package is correct.
+        internal static bool CheckUpgradeDetail(string upgradeFolder)
+        {
+            var releaseNotes = Path.Combine(upgradeFolder, UpgradePackageUltility.ReleaseNoteFile);
+            if (File.Exists(releaseNotes))
+            {
+                UpgradePackage activePackage;
+                var packages = UpgradePackageUltility.LoadUpgradePackagesReleaseNotes(releaseNotes, out activePackage);
+
+                if (packages != null && packages.Count > 0)
+                {
+                    if (activePackage != null && !string.IsNullOrEmpty(activePackage.PackageName))
+                    {
+                        var packageFile = Path.Combine(upgradeFolder,
+                            string.Format("{0}{1}", activePackage.PackageName,
+                                UpgradePackageUltility.PackageFileExtension));
+
+                        if (File.Exists(packageFile))
+                            return true;
+                    }
+                } 
+            }
+            else
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        internal static void ReleaseToLocal(string zipFilePath)
+        {
+            //Copy and release new upgrade zip file.
+            var localZipFilePath = Path.Combine(@"D:\", Path.GetFileName(zipFilePath));
+            File.Copy(zipFilePath, localZipFilePath, true);
+
+            CleanAndRelease(localZipFilePath, @"D:\");
+            File.Delete(localZipFilePath);
+        }
+
+        /// <summary>
+        ///     Figure out if there is any suitable upgrade package zip inside USB. If so, send out the package path.
+        /// </summary>
+        /// <param name="usbPath">The usb root path.</param>
+        /// <param name="currentVersion">Version of current system.</param>
+        /// <param name="zipFilePath">Send out the upgrade package zip file path.</param>
+        /// <returns></returns>
+        internal static bool IsUpgradePackageExists(string usbPath, string currentVersion, out string zipFilePath)
+        {
+            //Find all zip files in USB, only search top directory.
+            var zipFileList = Directory.GetFiles(usbPath, "*.zip").ToList();
+            if (zipFileList.Count == 0)
+            {
+                zipFilePath = string.Empty;
+                return false;
+            }
+
+            //Remove zip which is not upgrade package.
+            foreach (var zipFile in zipFileList)
+            {
+                var version = GetVersion(zipFile);
+                if (string.IsNullOrEmpty(version) || version.Split('.', '(', ')').Length != 3 &&
+                    version.Split('.', '(', ')').Length != 5)
+                {
+                    zipFileList.Remove(zipFile);
+                }
+            }
+
+            if (zipFileList.Count == 0)
+            {
+                zipFilePath = string.Empty;
+                return false;
+            }
+
+            //Loop zip file list.
+            zipFileList.Sort((left, right) =>
+                string.Compare(CreateStandardVersion(GetVersion(left)),
+                    CreateStandardVersion(GetVersion(right)), StringComparison.OrdinalIgnoreCase));
+
+            //Do for loop here, zipFileList is ascending.
+            foreach (var zipFile in zipFileList)
+            {
+                if (string.Compare(CreateStandardVersion(GetVersion(zipFile)), CreateStandardVersion(currentVersion),
+                        StringComparison.OrdinalIgnoreCase) <= 0) continue;
+
+                if (!IsCorrectFormat(zipFile)) continue;
+
+                zipFilePath = zipFile;
+                return true;
+            }
+
+            zipFilePath = string.Empty;
+            return false;
+        }
+
+        /// <summary>
+        /// Check the content of upgrade zip without releasing it.
+        /// </summary>
+        /// <param name="zipFile">Zip file to be checked.</param>
+        private static bool IsCorrectFormat(string zipFile)
+        {
+            var version = GetVersion(zipFile);
+            using (var zip = ZipFile.OpenRead(zipFile))
+            {
+                //Check file name inside zip file.
+                var isDatFileExists = zip.Entries.Any(e => e.FullName.Contains(@"\Upgrade\" + version + @".dat"));
+                var isResourceNoteExists = zip.Entries.Any(e => e.FullName.Contains(@"\Upgrade\" + @"ReleaseNotes.xml"));
+
+                if (!isDatFileExists || !isResourceNoteExists) return false;
+            }
+
+            return true;
+        }
+
+        internal static string GetVersion(string filePath)
+        {
+            return Path.GetFileNameWithoutExtension(filePath);
+        }
+
+        /// <summary>
+        ///     Only support version format xx.xx.xx right now.
+        /// </summary>
+        /// <param name="normalVer">The version number it should be as usual.</param>
+        /// <returns>Returns the version number which is convenient to make string comparison.</returns>
+        internal static string CreateStandardVersion(string normalVer)
+        {
+            var splitVer = normalVer.Split('.', '(', ')');
+
+            if (splitVer.Length != 3 && splitVer.Length != 5) return string.Empty;
+
+            if (splitVer[0].Length == 1) splitVer[0] = "0" + splitVer[0];
+            if (splitVer[1].Length == 1) splitVer[1] = "0" + splitVer[1];
+            if (splitVer[2].Length == 1) splitVer[2] = "0" + splitVer[2];
+
+            switch (splitVer.Length)
+            {
+                case 3:
+                {
+                    var result = splitVer[0] + "." + splitVer[1] + "." + splitVer[2];
+                    return result;
+                }
+                case 5:
+                {
+                    var result = splitVer[0] + "." + splitVer[1] + "." + splitVer[2] +
+                                 string.Format(@"({0})", splitVer[3]);
+                    return result;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        //Clean up local folder, then release current upgrade package.
+        private static void CleanAndRelease(string zipFilePath, string targetPath)
+        {
+            //Upgrade package not found, do nothing.
+            if (string.IsNullOrEmpty(zipFilePath) || string.IsNullOrEmpty(targetPath)) return;
+
+            CleanUpgradePackage(targetPath);
+
+            //Unpack the upgrade package zip to USB.
+            UpgradePackageUltility.ExtractToDirectory(zipFilePath, targetPath);
+        }
+
+        private static void CleanUpgradePackage(string localPath)
+        {
+            var targetPath = Path.Combine(localPath, "");
+            Directory.Delete(targetPath, true);
+        }
+    }
+}
